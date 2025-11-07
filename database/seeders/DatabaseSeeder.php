9b79e7ae292e9911293a4353762dd5551b6eae3c
HM Zeeshan <engr.hmzeeshan@gmail.com>
2025-11-07 06:29:09 +0500

    Migration and Factory and Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call(CategorySeeder::class);
+        // $names = [
+        //      'Clothing',
+        //      'Cosmetics',
+        //      'Fashion'
+        // ];
+        // foreach($names as $n)
+        // {
+        //     Category::create(['name' =>$n]);
+        // }
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory(10)->create();
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
